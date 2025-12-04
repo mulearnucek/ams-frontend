fa3901309193046d7da4970d618602d178de0759
prince-of-death <developer.acc.2006@gmail.com>
2025-12-04 18:22:25 +0530

    feat: redirect to dashboard if signedin from the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,37 @@
 import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
-  const { data: session, isPending } = authClient.useSession();
+    const { data: session, isPending } = authClient.useSession();
+    const router = useRouter();
 
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="w-full flex justify-end mb-8">
-            {isPending ? (
-                <div className="text-sm text-zinc-500">Loading session...</div>
-            ) : session ? (
-                <div className="flex items-center gap-4">
-                    <div className="text-sm">
-                        Logged in as <span className="font-bold">{session.user.email}</span>
-                    </div>
-                    <Button 
-                        variant="outline" 
-                        onClick={async () => {
-                            await authClient.signOut();
-                        }}
-                    >
-                        Sign Out
-                    </Button>
-                </div>
-            ) : (
+    useEffect(() => {
+        if (session != null && !isPending) {
+            router.replace("/dashboard");
+        }
+    }, [session, isPending, router]);
+
+    if (isPending || session != null) {
+        return (
+            <div className="flex min-h-screen items-center justify-center">
+                <Loader2 className="h-8 w-8 animate-spin" />
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+            <div className="w-full flex justify-end mb-8">
                 <Link href="/signin">
                     <Button>Sign In</Button>
                 </Link>
-            )}
+            </div>
+        </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
